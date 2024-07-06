a61d1748aaf9461947aec3bcf8566be15f9af455
= <manhndhe170523@fpt.edu.vn>
2024-07-06 08:09:13 +0700

    feature: marker with latlng, current marker, current polyline

--- app/src/main/java/com/example/googlemapdemo/MapsActivity.java
+++ app/src/main/java/com/example/googlemapdemo/MapsActivity.java
@@ -4,18 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.graphics.Point;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -26,33 +23,20 @@ import android.widget.EditText;
 import android.widget.SearchView;
 import android.widget.Toast;
 
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.RetryPolicy;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
+
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.googlemapdemo.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -65,9 +49,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Location currentLocation;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private SearchView mapSearchView;
-    private static final String API_STRING = "AIzaSyDQRxpdhh_FZteuSyUJvUWxIbIHuTCv8IU"; // Replace with your actual API key
     private static final String TAG = "MapsActivity";
     private DrawerLayout drawerLayout;
+    private Marker currentMarker;
+    private Polyline currentPolyline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,22 +72,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                String location = mapSearchView.getQuery().toString();
-                List<Address> addressList = new ArrayList<>();
-                if (location != null) {
-                    Geocoder geocoder = new Geocoder(MapsActivity.this);
-                    try {
-                        addressList = geocoder.getFromLocationName(location, 1);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    if (!addressList.isEmpty()) {
-                        Address address = addressList.get(0);
-                        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                        mMap.addMarker(new MarkerOptions().position(latLng).title(location));
-                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                    }
-                }
+                searchLocation(query);
                 return false;
             }
 
@@ -111,36 +81,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 return false;
             }
         });
-
-
-        // Setup Navigation Drawer
-        drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.navigation_view);
-
-        // Setup ActionBarDrawerToggle
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        // Access the header view from NavigationView
-        View headerView = navigationView.getHeaderView(0);
-        if (headerView == null) {
-            Log.e("MapsActivity", "Header view is null");
-        } else {
-            EditText editTextOne = headerView.findViewById(R.id.et_from_location);
-            EditText editTextTwo = headerView.findViewById(R.id.et_to_location);
-            Button buttonGetDirectionsDrawer = headerView.findViewById(R.id.button_get_directions);
-
-            if (buttonGetDirectionsDrawer == null) {
-                Log.e("MapsActivity", "Button in header view is null");
-            } else {
-                buttonGetDirectionsDrawer.setOnClickListener(v -> {
-                    direction(editTextOne != null ? editTextOne.getText().toString() : "",
-                            editTextTwo != null ? editTextTwo.getText().toString() : "");
-                });
-            }
-        }
+        setupNavigationDrawer();
     }
 
     private void setSupportActionBar(Toolbar toolbar) {
@@ -150,42 +91,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             toolbar.setOnMenuItemClickListener(this::onOptionsItemSelected);
         }
     }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        if (currentLocation != null) {
-            LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15));
-            MarkerOptions options = new MarkerOptions().position(currentLatLng).title("My location");
-            options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
-            mMap.addMarker(options);
-
-            mMap.getUiSettings().setZoomControlsEnabled(true);
-            mMap.getUiSettings().setCompassEnabled(true);
-        }
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == FINE_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLastLocation();
-            } else {
-                Toast.makeText(this, "Permission is denied", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
@@ -203,17 +113,97 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         return super.onOptionsItemSelected(item);
     }
-
-    private void getLastLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
-            return;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == FINE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            } else {
+                Toast.makeText(this, "Permission is denied", Toast.LENGTH_SHORT).show();
+            }
         }
+    }
+    private void checkAndRequestLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+        }
+    }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        checkAndRequestLocationPermission();
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMapClickListener(this::handleMapClick);
+        setupMapUI();
+    }
+    private void handleMapClick(LatLng latLng) {
+        if (currentMarker != null) {
+            currentMarker.remove();
+        }
+        currentMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Vị trí chọn"));
+        String toastMsg = String.format("Kinh độ: %s, Vĩ độ: %s", latLng.longitude, latLng.latitude);
+        Toast.makeText(MapsActivity.this, toastMsg, Toast.LENGTH_LONG).show();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
+    }
+    private void setupMapUI() {
+        if (currentLocation != null) {
+            LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 20));
+        }
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+    }
+    private void searchLocation(String location) {
+        if (location != null && !location.isEmpty()) {
+            List<Address> addressList = new ArrayList<>();
+            try {
+                addressList = new Geocoder(MapsActivity.this).getFromLocationName(location, 1);
+                if (!addressList.isEmpty()) {
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    updateMapLocation(latLng, location);
+                } else {
+                    Toast.makeText(MapsActivity.this, "Địa điểm không được tìm thấy", Toast.LENGTH_SHORT).show();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void updateMapLocation(LatLng latLng, String location) {
+        if (currentMarker != null) {
+            currentMarker.remove();
+        }
+        currentMarker = mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+    }
+    private void setupNavigationDrawer() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, findViewById(R.id.toolbar), R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        View headerView = navigationView.getHeaderView(0);
+        if (headerView != null) {
+            Button buttonGetDirectionsDrawer = headerView.findViewById(R.id.button_get_directions);
+            if (buttonGetDirectionsDrawer != null) {
+                buttonGetDirectionsDrawer.setOnClickListener(v -> {
+                    String fromLocation = ((EditText) headerView.findViewById(R.id.et_from_location)).getText().toString();
+                    String toLocation = ((EditText) headerView.findViewById(R.id.et_to_location)).getText().toString();
+                    direction(fromLocation, toLocation);
+                });
+            }
+        }
+    }
+    private void getLastLocation() {
+        checkAndRequestLocationPermission();
         Task<Location> task = fusedLocationProviderClient.getLastLocation();
         task.addOnSuccessListener(location -> {
             currentLocation = location;
-
             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
             if (mapFragment != null) {
                 mapFragment.getMapAsync(MapsActivity.this);
@@ -223,25 +213,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Toast.makeText(this, "Failed to get location", Toast.LENGTH_SHORT).show();
         });
     }
+    private void direction(String locationFrom, String locationTo) {
+        LatLng point1 = getPosition(locationFrom);
+        LatLng point2 = getPosition(locationTo);
 
-    private void direction(String locationFrom, String locationTo){
-        LatLng point1 = null;
-        LatLng point2 = null;
-        if(!locationFrom.contains(",") || !locationTo.contains(",")){
-            // Thêm hai điểm LatLng
-            point1 = getPosition(locationFrom); // Ví dụ: Tọa độ điểm 1
-            point2 = getPosition(locationTo); // Ví dụ: Tọa độ điểm 2
-        } else{
-            String[] pointOne = locationFrom.split(",");
-            String[] pointTwo = locationTo.split(",");
+        // Check if either point is null and notify the user if so
+        if (point1 == null || point2 == null) {
+            Toast.makeText(this, "Một hoặc hai vị trí không được tìm thấy", Toast.LENGTH_SHORT).show();
+            return; // Exit the method early to prevent NullPointerException
+        }
 
-            // Thêm hai điểm LatLng
-            point1 = new LatLng(Double.parseDouble(pointOne[0].trim()), Double.parseDouble(pointOne[1].trim())); // Ví dụ: Tọa độ điểm 1
-            point2 = new LatLng(Double.parseDouble(pointTwo[0].trim()), Double.parseDouble(pointTwo[1].trim())); // Ví dụ: Tọa độ điểm 2
+        // Xóa đường thẳng cũ nếu có
+        if (currentPolyline != null) {
+            currentPolyline.remove();
         }
 
         // Vẽ đường thẳng giữa hai điểm và đặt màu cho đường thẳng
-        Polyline polyline = mMap.addPolyline(new PolylineOptions()
+        currentPolyline = mMap.addPolyline(new PolylineOptions()
                 .clickable(true)
                 .add(point1, point2)
                 .color(Color.parseColor("#9e4f34"))); // Đổi màu đường thẳng
@@ -249,7 +237,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Di chuyển camera đến điểm đầu tiên và zoom
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point1, 15));
     }
-    public LatLng getPosition(String location){
+    public LatLng getPosition(String location) {
         List<Address> addressList = new ArrayList<>();
         if (location != null) {
             Geocoder geocoder = new Geocoder(MapsActivity.this);
@@ -265,4 +253,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         return null;
     }
+
+    //    private void direction(String locationFrom, String locationTo) {
+//        LatLng point1 = null;
+//        LatLng point2 = null;
+//        if (!locationFrom.contains(",") || !locationTo.contains(",")) {
+//            // Thêm hai điểm LatLng
+//            point1 = getPosition(locationFrom); // Ví dụ: Tọa độ điểm 1
+//            point2 = getPosition(locationTo); // Ví dụ: Tọa độ điểm 2
+//        } else {
+//            String[] pointOne = locationFrom.split(",");
+//            String[] pointTwo = locationTo.split(",");
+//
+//            // Thêm hai điểm LatLng
+//            point1 = new LatLng(Double.parseDouble(pointOne[0].trim()), Double.parseDouble(pointOne[1].trim())); // Ví dụ: Tọa độ điểm 1
+//            point2 = new LatLng(Double.parseDouble(pointTwo[0].trim()), Double.parseDouble(pointTwo[1].trim())); // Ví dụ: Tọa độ điểm 2
+//        }
+//
+//        // Vẽ đường thẳng giữa hai điểm và đặt màu cho đường thẳng
+//        Polyline polyline = mMap.addPolyline(new PolylineOptions()
+//                .clickable(true)
+//                .add(point1, point2)
+//                .color(Color.parseColor("#9e4f34"))); // Đổi màu đường thẳng
+//
+//        // Di chuyển camera đến điểm đầu tiên và zoom
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point1, 15));
+//    }
 }
